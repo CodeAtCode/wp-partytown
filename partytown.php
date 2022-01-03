b6f88162947d453989701d870de717fb641bd376
Daniele Scasciafratte <mte90net@gmail.com>
2022-01-03 15:25:13 +0100

    feat(partytown): updated to latest relase

--- partytown.php
+++ partytown.php
@@ -17,9 +17,9 @@ if ( !defined( 'ABSPATH' ) ) {
 function enqueue_partytown() {
 	?>
 <script>
-	window.partytown = {lib:"<?php echo plugins_url( 'partytown/assets/', plugin_dir_path( __FILE__ ) ) ?>"};
-	!function(n,t){var r=n.partytown||{},e=r.forward||[],p=t.createElement("script");function u(t){return function(){(n._ptf=n._ptf||[]).push(t,arguments)}}e.map((t=>{n[t[0]]=1===t[1]?function(n,t){return(t=[]).push=u(n),t}(t):u(t)})),p.async=p.defer=!0,p.src=(r.lib||"/~partytown/")+(r.debug?"debug/":"")+"partytown.js",t.head.appendChild(p)}(window,document);
+	window.partytown = {lib:"<?php echo plugins_url( 'wp-partytown/assets/', plugin_dir_path( __FILE__ ) ) ?>"};
 </script>
+<script src="<?php plugins_url( 'wp-partytown/assets/', plugin_dir_path( __FILE__ ) ) ?>/partytown.js"></script>
 	<?php
 }
 add_action('wp_print_scripts', 'enqueue_partytown', -1);
